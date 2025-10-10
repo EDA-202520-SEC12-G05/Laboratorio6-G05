6096abfa0a53ce6006b374aef88b83863a37a363
Fzzzog77 <fergyeortizgomez@gmail.com>
2025-10-09 20:28:54 -0500

    Creado new_map en map_separate_chaining

--- DataStructures/Map/map_linear_probing.py
+++ DataStructures/Map/map_linear_probing.py
@@ -1,6 +1,1 @@
-
-import map_entry as me
-import map_functions as mf
-
-def new_map(num_elements, load_factor, prime=109345121):
- pass
+from DataStructures.Map import map_functions as mf
--- DataStructures/Map/map_separate_chaining.py
+++ DataStructures/Map/map_separate_chaining.py
@@ -1,3 +1,20 @@
-import map_entry as me
-import map_functions as mf
+from DataStructures.Map import map_functions as mf
+from DataStructures.List import array_list as al
+from DataStructures.List import single_linked_list as sll
+import random as rd
 
+def new_map(num_elements, load_factor, prime=109345121):
+    capacity = mf.next_prime(num_elements//load_factor)
+    scale = rd.randint(1, prime-1)
+    shift = rd.randint(0, prime-1)
+    htable = {'prime': prime,
+    'capacity': capacity,
+    'scale': scale,
+    'shift': shift,
+    'table': al.new_list(),
+    'size': 0,
+    'limit_factor': load_factor,
+    'current_factor': 0}
+    for _ in range(capacity):
+        al.add_last(htable['table'], sll.new_list())
+    return htable
